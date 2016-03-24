3c10cde20ad80139df377faa575b07480337bdf2
Roland Burrows <rolandnburrows@gmail.com>
2016-03-23 23:13:56 -0600

    Prep tesseract-ocr for prime time
    gem install cosmic-cube

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "rspec"
+gem "tesseract-ocr"
 gem "codeclimate-test-reporter", group: :test, require: nil
---
+++ sudokocr.rb
@@ -0,0 +1,18 @@
+# Sudokocr
+# Ruby optical character recognition Sudoku puzzle hyper-extractor
+
+# Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| require(f) }
+
+# encoding: utf-8
+require 'tesseract'
+
+engine = Tesseract::Engine.new do |config|
+  config.language  = :eng
+  config.blacklist = '|'
+end
+
+def clean(text)
+  text.split(/\n/).compact.select { |v| v.size > 0 }
+end
+
+puts clean(e.text_for(ARGV.first))
