f39b3fe8ef9d10de9176b7db1f2c258c26a8fa5f
Roland Burrows <rolandnburrows@gmail.com>
2016-03-27 14:03:26 -0600

    Activate codeclimate reporter and refactor Main
    Evolution… complete.
    The hive cluster is under attack!

--- sudokocr.rb
+++ sudokocr.rb
@@ -6,8 +6,19 @@ Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| require(f) }
 # Gather input
 input_image = ARGV[0] 
 
-# Spin up the tesseract OCR
-engine = Engine.new(filter=true)
+class Sudokocr
+  # Spin up the tesseract OCR
+  def initialize
+    @engine = Engine.new(filter=true)
+  end
+
+  def extract_text(image)
+    extraction = @engine.extract_text(image)
+    puts extraction
+    return extraction
+  end
+end
 
 # Display results
-puts engine.extract_text(input_image)
+sudo = Sudokocr.new
+sudo.extract_text(input_image)
